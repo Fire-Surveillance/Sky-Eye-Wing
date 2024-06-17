38c86a877e9d6c7ca3230925e41059ed4dcb0eb2
William Yuan <86529144+Fire-Surveillance@users.noreply.github.com>
2024-06-17 14:29:48 -0800

    Update code.py

--- Sky Eye Wing 2024/Camera/code.py
+++ Sky Eye Wing 2024/Camera/code.py
@@ -7,82 +7,64 @@ import digitalio
 import adafruit_mlx90640
 import adafruit_rfm9x
 
-
+# Initialize RFM9x LoRa module
 spi = board.SPI()
-
 cs = digitalio.DigitalInOut(board.D5)
 reset = digitalio.DigitalInOut(board.D6)
-
 rfm9x = adafruit_rfm9x.RFM9x(spi, cs, reset, 915.0)
 
-
+# Initialize I2C communication for MLX90640 thermal camera
 i2c = busio.I2C(board.SCL, board.SDA, frequency=800000)
-
 mlx = adafruit_mlx90640.MLX90640(i2c)
 print("MLX addr detected on I2C")
 print([hex(i) for i in mlx.serial_number])
 
-# mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ
-mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_4_HZ
+# Set refresh rate of MLX90640
+mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_8_HZ
 
-
+# Function to convert frame to a message format
 def imageToMessage(frame):
     s = ""
     for i in frame:
         s = s + str(round(i)) + ","
-        # s = s + str(chr(round(i)))
-    return str(1)
     return s[:-1]
-    return "" + str(chr(round(frame[0])))
 
+
+# Initialize a frame buffer
 frame = [0] * 768
 
+# Function to send image frame over RFM9x
 def sendImage(frame):
     imageSize = 768
     packets = 16
     packetLength = imageSize // packets
 
-    count = 0
     for i in range(packets):
         print(i)
         j = i * packetLength
-        subFrame = frame[j:j+packetLength]
+        subFrame = frame[j : j + packetLength]
         message = str(i) + ":"
         for k in subFrame:
-            message = message + str(round(k)) + ','
-        #print(message)
+            message = message + str(round(k)) + ","
         rfm9x.send(message[:-1])
-
-    # for i in frame:
-
-    #     message = str(count) + ": " + str(round(i))
-    #     count += 1
-    #     # print('sending: ' + message)
-    #     rfm9x.send(message)
-    print('sent')
+    print("Image frame sent")
 
 
-
-start_time = time.monotonic() - 10
+# Main loop to continuously capture and send frames
+start_time = time.monotonic() - 1  # Start with a smaller initial time difference
 stamp = time.monotonic()
 
 while True:
     stamp = time.monotonic()
-    print(stamp)
+    print("Current timestamp:", stamp)
 
-    if (stamp-start_time) > 5:
-
+    # Check if it's time to capture a new frame (every 1 second)
+    if (stamp - start_time) > 1:
         try:
             mlx.getFrame(frame)
         except ValueError:
-            # these happen, no biggie - retry
+            # Handle potential errors in frame acquisition
             continue
-
-        #    print("Time for data aquisition: %0.2f s" % (time.monotonic()-stamp))
-
-
-
-        print('sending')
-
+        print("Sending image frame...")
         sendImage(frame)
-        start_time = stamp
+        start_time = stamp  # Update start time for next frame capture
